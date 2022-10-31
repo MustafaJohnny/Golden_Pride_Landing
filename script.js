25b2b400837476add44e7449e8450ac09237f5b1
Mustafa Johnny <mustafajohnn99@gmail.com>
2022-10-31 13:27:59 +0300

    contact modal is added

--- script.js
+++ script.js
@@ -39,3 +39,29 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+
+// Selecting the elements in oroder to make the contact pop up works!
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
+
+// Simple function can show the modal when the user clicks on one of the modal buttons.
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// Almost the same logic of the previous function but to close the modal.
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// Calling the openModal function on a click event.
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+// Calling the closeModal function on click and also when the user clicks anywhere on the overlay.
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
